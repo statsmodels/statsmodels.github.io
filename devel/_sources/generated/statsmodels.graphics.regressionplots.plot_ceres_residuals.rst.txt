1d7f626576050f3c95dc6535952ba9ce872e9b98
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-05-03 01:54:47 +0000

    Update docs after building Travis build 9418 of statsmodels/statsmodels
    The docs were built from the branch 'master' against the commit
    28609c07e4f2543f1148515e902d95ecb56a9684.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/statsmodels/statsmodels/jobs/527524408.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/statsmodels-test/bin/doctr deploy --built-docs docs/build/html/ --deploy-repo statsmodels/statsmodels.github.io devel

---
+++ devel/_sources/generated/statsmodels.graphics.regressionplots.plot_ceres_residuals.rst.txt
@@ -0,0 +1,6 @@
+statsmodels.graphics.regressionplots.plot\_ceres\_residuals
+===========================================================
+
+.. currentmodule:: statsmodels.graphics.regressionplots
+
+.. autofunction:: plot_ceres_residuals
--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.10.0.dev0+1201.gcdf975106',
+    VERSION: 'v0.10.0.dev0+1208.g28609c07e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
