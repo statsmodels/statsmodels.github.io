003e3bd6096a2580fc7809572bd98bdbe469cdf1
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-06-11 22:34:47 +0000

    Update docs after building Travis build 10101 of statsmodels/statsmodels
    The docs were built from the branch 'master' against the commit
    975813e180c3b727e1d19ed40a54e0e343105dec.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/statsmodels/statsmodels/jobs/544435891.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/statsmodels-test/bin/doctr deploy --built-docs docs/build/html/ --deploy-repo statsmodels/statsmodels.github.io devel

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.10.0rc2+27.gdfdc2172c',
+    VERSION: 'v0.10.0rc2+33.g975813e18',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
