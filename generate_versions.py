cbf07e70f2fed75cd8cf46b8b44185952e013dfa
thequackdaddy <pquack@gmail.com>
2019-06-23 15:42:32 -0500

    DOC: update versions

--- generate_versions.py
+++ generate_versions.py
@@ -3,18 +3,28 @@
 import os
 import sys
 import json
-from distutils.version import StrictVersion
+from distutils.version import LooseVersion
+
+
+def sort_versions(versions):
+    stripped_version = [version[1:] if version[0] == 'v' else version
+                        for version in versions]
+    argsort = [x for x, y in sorted(enumerate(stripped_version),
+                                    key=lambda x: LooseVersion(x[1]))]
+    return [versions[i] for i in argsort]
+
 
 def main():
     versions = []
     for tmp in os.walk('.'):
         _, dirs, _ = tmp
         for directory in dirs:
-            if (directory not in ('dev', 'devel', 'stable') and
-                    directory[0] != '.'):
+            if (directory not in ('dev', 'devel', 'stable')
+                    and directory[0] != '.'):
                 versions.append(directory)
         break
-    versions.sort(key=StrictVersion)
+
+    versions = sort_versions(versions)
     versions = versions + ['stable', 'devel']
     with open('versions.json', 'w') as f:
         f.write(json.dumps(versions))
@@ -22,4 +32,3 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main())
-
